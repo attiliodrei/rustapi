81f5d705e91012ce2375a45e0aae0d73f0af94f7
Attilio Drei <attilio.drei@ies-italia.it>
2024-12-15 18:40:55 +0100

    bind all interfaces

--- src/main.rs
+++ src/main.rs
@@ -95,7 +95,7 @@ async fn main() -> std::io::Result<()> {
                     .route("/{id}", web::delete().to(delete_user))
             )
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
